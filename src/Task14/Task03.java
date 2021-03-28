811b7731ab9e854295d838db2c38996a6a1c31fe
EvilRockHuman <coursejava2021@gmail.com>
2021-03-28 15:58:23 +0300

    complete Task Four

---
+++ src/Task14/Task03.java
@@ -0,0 +1,22 @@
+package Task14;
+
+
+import java.util.*;
+
+public class Task03 {
+    public static void main(String[] args) {
+        Map<String, String> waitMe = new LinkedHashMap<>();
+        waitMe.put("Lara", "Petrov");
+        waitMe.put("Sergey", "Petrov");
+        waitMe.put("Maxim", "Baba");
+        waitMe.put("Petr", "Petrov");
+        waitMe.put("Mark", "Shewcov");
+        waitMe.put("Mir", "Mala");
+        waitMe.put("Lara", "Buba");
+        waitMe.put("Mur", "Mnogo");
+        waitMe.put("Tur", "Mala");
+        waitMe.put("Bar", "Buba");
+        Iterator<String> iterator = waitMe.keySet().iterator();
+        System.out.println(waitMe);
+    }
+}
