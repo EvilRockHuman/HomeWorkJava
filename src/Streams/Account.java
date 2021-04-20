c0825bf345bd543c63759aca3aa415e6f9004411
EvilRockHuman <coursejava2021@gmail.com>
2021-04-20 07:10:42 +0300

    finish

--- src/Streams/Account.java
+++ src/Streams/Account.java
@@ -1,23 +1,8 @@
 package Streams;
 
-import java.io.Serializable;
+public class Account {
+    String username;
+    String email;
+    static String password;
 
-public class Account implements Serializable {
-     String username;
-     String email;
-     transient String password;
-
-    public Account(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
--- src/Streams/lesson4.java
+++ src/Streams/lesson4.java
@@ -2,18 +2,20 @@ package Streams;
 
 import java.io.*;
 
-public class lesson4 implements Serializable {
-    public static void main(String[] args) {
-        File file = new File("C:\\Users\\Матвей СНиИ\\Desktop\\идеа\\HomeWorkJava\\src\\Streams\\lesson4.txt");
-        Account account = new Account("Simkora","job_it@gmail.com","12345678");
-        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
-            out.writeObject(account);
-            out.flush();
-            Account account1 = (Account) ois.readObject();
-            System.out.println(account1);
-        } catch (IOException | ClassNotFoundException e){
-            System.err.println(false);
+public class lesson4 extends Account implements Serializable {
+    FileOutputStream fos;
+    ObjectOutputStream oos;
+    {
+        try {
+            fos = new FileOutputStream("C:\\Users\\User\\IdeaProjects\\HomeWorkJava\\src\\Streams\\file\\lesson4.txt");
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(username);
+            oos.writeObject(email);
+            oos.writeObject(password);
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
 }
